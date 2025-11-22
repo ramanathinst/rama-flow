30f4b916b5cf3589c7be1c9e57d350781afd1ff0
Ramanath <ramanathinst@gmail.com>
2025-11-22 17:36:47 +0530

    08: Error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,7 @@ import prisma from "@/lib/db";
 import { inngest } from "./client";
 import { generateText } from "ai"
 import { createGoogleGenerativeAI } from "@ai-sdk/google"
+import * as Sentry from "@sentry/nextjs";
 
 const google = createGoogleGenerativeAI({
   // custom settings
@@ -11,12 +12,20 @@ export const workflow = inngest.createFunction(
     { id: "ai-execute" },
     { event: "ai/execute" },
     async ({ event, step }) => {
+
+        Sentry.logger.info("user log is step", { Log_source: "ai_execute"})
+
         await step.sleep("wait-a-moment", "5s");
         
         const {steps} = await step.ai.wrap("ai-execute",generateText, {
             model: google("gemini-2.5-flash"),
             system: "You are helpful assistent",
-            prompt: "what is 4+9?"
+            prompt: "what is 4+9?",
+            experimental_telemetry: {
+                isEnabled: true,
+                recordInputs: true,
+                recordOutputs: true
+            }
         })
         return {steps}  
     },
