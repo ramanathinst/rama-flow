8586a5de61b244860a5370210166914b6971f1d0
Ramanath <ramanathinst@gmail.com>
2025-12-09 07:49:07 +0530

    12: Workflows Pagination

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,5 +1,6 @@
+import { PAGINATION } from "@/config/constants";
 import prisma from "@/lib/db";
-import { createTRPCRouter, premiumProcedure } from "@/trpc/init";
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import { id } from "date-fns/locale";
 import { generateSlug } from "random-word-slugs"
 import z from "zod";
@@ -40,11 +41,52 @@ export const workflowsRouter = createTRPCRouter({
             }
         })
     }),
-    getMany: premiumProcedure.query(async({ctx}) => {
-        return prisma.workflow.findMany({
-            where: {
-                userId: ctx.auth.user.id,
-            }
+    getMany: protectedProcedure.input(
+        z.object({
+            page: z.number().min(1).default(PAGINATION.DEFAULT_PAGE),
+            pageSize: z.number().max(PAGINATION.MAX_PAGE_SIZE)
+            .min(PAGINATION.MIN_PAGE_SIZE)
+            .default(PAGINATION.DEFAULT_PAGE_SIZE),
+            search: z.string().default("")
         })
+    ).query(async({ctx, input}) => {
+        const { page, pageSize, search}  = input;
+        const [ items, totalCount ] = await Promise.all([
+            prisma.workflow.findMany({
+                skip: (page - 1) * pageSize,
+                take: pageSize,
+                where: {
+                    userId: ctx.auth.user.id,
+                    name: {
+                        contains: search,
+                        mode: "insensitive"
+                    }
+                },
+                orderBy: {
+                    updatedAt: "desc"
+                }
+            }),
+            prisma.workflow.count({
+                where: {
+                    userId: ctx.auth.user.id,
+                    name: {
+                        contains: search
+                    }
+                }
+            })
+        ])
+
+        const totalPages = Math.ceil(totalCount / pageSize);
+        const hasNextPage = page < pageSize;
+        const hasPreviousPage = page > 1;
+
+        return {
+            items,
+            page,
+            pageSize,
+            totalPages,
+            hasNextPage,
+            hasPreviousPage
+        }
     })
 });
